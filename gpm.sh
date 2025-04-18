e6369e07171593ee6c36e3122cf182c8e90eacf5
BasilYes <basilyevs@gmail.com>
2025-04-18 17:18:36 +0300

    test install as clone

--- gpm.sh
+++ gpm.sh
@@ -49,13 +49,19 @@ install () {
             if [[ -z $i ]];then continue; fi
             IFS=' '
             split=($i)
-            if [[ -f .submodules ]] && ([[ -z $(cat .submodules | grep split[1]) ]] || [[ -z $(cat .submodules | grep split[2]) ]]); then continue; fi
-            read -p "install ${split[1]} to ${split[0]} y/n:" install
-            if [[ "$install" == "y" ]] || [[ "$install" == "yes" ]]; then
-                if [[ -z selected ]]; then
-                    selected="${i}"
+            if [[ -f .submodules ]] && ([[ -z $(cat .submodules | grep ${split[1]}) ]] || [[ -z $(cat .submodules | grep ${split[2]}) ]]); then continue; fi
+            # Get the default method from the installlist (3rd column) or use "m" if not specified
+            default_method="${split[3]:-m}"
+            read -p "install ${split[1]} to ${split[0]} using sub(m)odule or (c)lone enter for ${default_method} [m/c/n]:" install
+            # If user just pressed enter, use the default method
+            if [[ -z "$install" ]]; then
+                install="$default_method"
+            fi
+            if [[ "$install" == "m" ]] || [[ "$install" == "c" ]]; then
+                if [[ -z $selected ]]; then
+                    selected="${split[0]} ${split[1]} ${split[2]} $install"
                 else
-                    selected="${selected}${NL}${i}"
+                    selected="${selected}${NL}${split[0]} ${split[1]} ${split[2]} $install"
                 fi
             fi
         done
@@ -67,15 +73,44 @@ install () {
     for i in $selected;do
         IFS=' '
         split=($i)
-        if [[ -f .submodules ]] && ([[ -z $(cat .submodules | grep split[1]) ]] || [[ -z $(cat .submodules | grep split[2]) ]]); then continue; fi
+        if [[ -f .submodules ]] && ([[ -z $(cat .submodules | grep ${split[1]}) ]] || [[ -z $(cat .submodules | grep ${split[2]}) ]]); then continue; fi
         IFS='.'
         name=(${split[1]})
         IFS='/'
         name=(${name[-2]})
         name=${name[-1]}
         path="${split[0]}/${name}"
-        git "submodule" "add" "${split[1]}" "$path" ||
-        git "submodule" "add" "${split[2]}" "$path"
+
+        # Check if addon is already installed
+        if [[ -d "$path" ]]; then
+            echo "Addon already installed at $path, skipping..."
+            continue
+        fi
+
+        # Default to submodule if method not specified
+        method=${split[3]:-"m"}
+
+        if [[ "$method" == "m" ]]; then
+            # Install as submodule
+            git "submodule" "add" "${split[1]}" "$path" ||
+            git "submodule" "add" "${split[2]}" "$path"
+            commit_msg="add submodule"
+        elif [[ "$method" == "c" ]]; then
+            # Install as clone (instead of subtree) - only get the last commit
+            repo_url=${split[1]}
+            # Try the first URL, if it fails try the second URL
+            if ! git clone --depth 1 "${repo_url}" "$path"; then
+                git clone --depth 1 "${split[2]}" "$path"
+            fi
+            
+            # Remove git directory to detach from original repo
+            rm -rf "$path/.git"
+            
+            # Add the directory to our repo
+            git add "$path"
+            commit_msg="add clone"
+        fi
+
         commit="${commit}${NL}${path}"
         path="./${path}/dependencies"
         if [[ -f $path ]];then
@@ -85,7 +120,7 @@ install () {
 
     if [[ -n $selected ]]; then
         git add .
-        git commit -m "$(echo -e "add submodules\n${commit}")"
+        git commit -m "$(echo -e "${commit_msg}\n${commit}")"
     fi
 }
 
@@ -98,5 +133,14 @@ if [[ "$1" = "init" ]];then
 elif [[ "$1" = "dirs" ]];then
     create_dirs
 elif [[ "$1" = "install" ]];then
-    install "$(cat ./installlist | grep $2)"
+    # If method is explicitly provided, use it
+    if [[ "$3" = "clone" ]] || [[ "$3" = "c" ]]; then
+        method="c"
+    elif [[ "$3" = "submodule" ]] || [[ "$3" = "m" ]]; then
+        method="m"
+    else
+        # If no method specified in command line, use the one from installlist
+        method=""
+    fi
+    install "$(cat ./installlist | grep $2) $method"
 fi
